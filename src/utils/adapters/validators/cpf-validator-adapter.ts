199d186deb1e233650a85242177f1e69f88a4472
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 16:13:29 -0400

    feat: ensure CpfValidatorAdapter calls cpf-check validate method with valid cpf

---
+++ src/utils/adapters/validators/cpf-validator-adapter.ts
@@ -0,0 +1,9 @@
+import { CpfValidator } from '@cpf-validator'
+import CPF from 'cpf-check'
+
+export class CpfValidatorAdapter implements CpfValidator {
+  async isCpfValid (cpf: string): Promise<boolean> {
+    CPF.validate(cpf)
+    return await new Promise(resolve => resolve(true))
+  }
+}
