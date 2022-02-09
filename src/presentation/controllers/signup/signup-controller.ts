826998decc4702a1d3e5fd884565b4e02babda11
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 11:59:11 -0400

    feat: ensure SignUpController return 400 if no birthDate is provided

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -3,7 +3,7 @@ import { HttpRequest, httpResponse } from '../../protocols/http/http'
 
 export class SignUpController {
   async handle (httpRequest: HttpRequest): Promise<httpResponse> {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation', 'telephone']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation', 'telephone', 'birthDate']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(field)
