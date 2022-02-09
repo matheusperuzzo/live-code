2c4aa35ea3e8f29f83bb545db1bfa4ebf72fa805
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 15:45:45 -0400

    feat: ensure TelephoneValidatorAdapter returns false if validation fails

--- src/utils/adapters/validators/telephone-validator-adapter.ts
+++ src/utils/adapters/validators/telephone-validator-adapter.ts
@@ -4,6 +4,6 @@ import validator from 'validator'
 export class TelephoneValidatorAdapter implements TelephoneValidator {
   async isTelephoneValid (telephone: string): Promise<boolean> {
     validator.isMobilePhone(telephone)
-    return await new Promise(resolve => resolve(true))
+    return await new Promise(resolve => resolve(false))
   }
 }
