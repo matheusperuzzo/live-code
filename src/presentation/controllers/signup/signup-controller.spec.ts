de83c4506cb42c65ffe5388284529ef3b96b7cec
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 11:57:15 -0400

    test: ensure SignUpController return 400 if no telephone is provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -69,4 +69,21 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest('passwordConfirmation'))
   })
+
+  test('Should return 400 if no telephone is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        birthDate: 'any_bith_date',
+        mothersName: 'any_mothers_name',
+        cpf: 'any_cpf'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest('telephone'))
+  })
 })
