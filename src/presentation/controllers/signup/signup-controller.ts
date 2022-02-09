873dc6675266e75be37f7bc15b8cfe17c906ab78
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 11:32:32 -0400

    refactor: remove duplicated code

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -2,22 +2,13 @@ import { HttpRequest, httpResponse } from '../../protocols/http/http'
 
 export class SignUpController {
   async handle (httpRequest: HttpRequest): Promise<httpResponse> {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
-      }
-    }
-    if (!httpRequest.body.password) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: password')
+    const requiredFields = ['name', 'email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
       }
     }
     return {
