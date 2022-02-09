988f41b5845a960772db6b226f236e1112bdd461
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 15:43:36 -0400

    feat: ensure TelephoneValidatorAdapter calls validator isMobilePhone method with valid telephone

---
+++ src/utils/adapters/validators/telephone-validator-adapter.ts
@@ -0,0 +1,9 @@
+import { TelephoneValidator } from '@presentation/protocols/signup/validators'
+import validator from 'validator'
+
+export class TelephoneValidatorAdapter implements TelephoneValidator {
+  async isTelephoneValid (telephone: string): Promise<boolean> {
+    validator.isMobilePhone(telephone)
+    return await new Promise(resolve => resolve(true))
+  }
+}
