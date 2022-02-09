95c394779348800d07294b6a636a5e084e6f55db
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 16:42:18 -0400

    test: ensure DbAddAccount calls Hasher with valid password

---
+++ src/data/usecases/account/db-add-account.spec.ts
@@ -0,0 +1,27 @@
+import { AddAccountModel } from '@domain/protocols/models/account'
+import { Hasher } from '../../protocols/cryptography/hasher'
+import { DbAddAccount } from './db-add-account'
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call Hasher with valid password', async () => {
+    class HasherStub implements Hasher {
+      async hash (value: string): Promise<string> {
+        return await new Promise(resolve => resolve('hash'))
+      }
+    }
+    const hasherStub = new HasherStub()
+    const sut = new DbAddAccount(hasherStub)
+    const hashSpy = jest.spyOn(hasherStub, 'hash')
+    const fakeAddAccount: AddAccountModel = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+      telephone: 'valid_telephone',
+      birthDate: 'valid_birth_date',
+      mothersName: 'valid_mothers_name',
+      cpf: 'valid_cpf'
+    }
+    await sut.add(fakeAddAccount)
+    expect(hashSpy).toHaveBeenCalledWith('valid_password')
+  })
+})
