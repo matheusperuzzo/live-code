c5163a433c6e848b3de20c58738667c921d9fcb8
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 14:47:40 -0400

    feat: ensure EmailValidatorAdapter returns false if validation fails

--- src/utils/adapters/validators/email-validator-adapter.ts
+++ src/utils/adapters/validators/email-validator-adapter.ts
@@ -4,6 +4,6 @@ import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
   async isEmailValid (email: string): Promise<boolean> {
     validator.isEmail(email)
-    return await new Promise(resolve => resolve(true))
+    return await new Promise(resolve => resolve(false))
   }
 }
