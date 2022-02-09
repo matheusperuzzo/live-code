15ca75ce5b80469c074c8d0aa480340e8cacfe7f
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 15:48:48 -0400

    test: ensure TelephoneValidatorAdapter returns true if validation succeeds

--- src/utils/adapters/validators/telephone-validator-adapter.spec.ts
+++ src/utils/adapters/validators/telephone-validator-adapter.spec.ts
@@ -11,7 +11,14 @@ describe('TelephoneValidator Adapter', () => {
 
   test('should return false if validation fails', async () => {
     const sut = new TelephoneValidatorAdapter()
-    const isValid = await sut.isTelephoneValid('invvalid_Telephone')
+    const isValid = await sut.isTelephoneValid('invalid_Telephone')
     expect(isValid).toBe(false)
+  })
+
+  test('should return true if validation succeeds', async () => {
+    const sut = new TelephoneValidatorAdapter()
+    jest.spyOn(validator, 'isMobilePhone').mockReturnValueOnce(true)
+    const isValid = await sut.isTelephoneValid('valid_Telephone')
+    expect(isValid).toBe(true)
   })
 })
