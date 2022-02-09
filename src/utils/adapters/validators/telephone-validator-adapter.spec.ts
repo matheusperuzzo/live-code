0946bc04850f3a1a34741b1ef7ce434b9445e630
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2022-02-09 15:46:11 -0400

    test: ensure TelephoneValidatorAdapter returns false if validation fails

--- src/utils/adapters/validators/telephone-validator-adapter.spec.ts
+++ src/utils/adapters/validators/telephone-validator-adapter.spec.ts
@@ -8,4 +8,10 @@ describe('TelephoneValidator Adapter', () => {
     await sut.isTelephoneValid('valid_Telephone')
     expect(isTelephoneSpy).toHaveBeenCalledWith('valid_Telephone')
   })
+
+  test('should return false if validation fails', async () => {
+    const sut = new TelephoneValidatorAdapter()
+    const isValid = await sut.isTelephoneValid('invvalid_Telephone')
+    expect(isValid).toBe(false)
+  })
 })
